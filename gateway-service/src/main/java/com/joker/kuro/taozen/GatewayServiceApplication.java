3e3cc784ecb346c8f2ff1da0c5cc62c4a8969256
lujc <lujc@inno-view.cn>
2019-12-04 13:23:02 +0800

    【摘要】网关开启负载均衡

--- gateway-service/src/main/java/com/joker/kuro/taozen/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/joker/kuro/taozen/GatewayServiceApplication.java
@@ -3,6 +3,9 @@ package com.joker.kuro.taozen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerInterceptor;
+import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -10,6 +13,11 @@ public class GatewayServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+    @Bean
+    LoadBalancerInterceptor loadBalancerInterceptor(LoadBalancerClient loadBalance) {
+        return new LoadBalancerInterceptor(loadBalance);
     }
 
 }
