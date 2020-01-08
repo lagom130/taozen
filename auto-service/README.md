45c202f9272a5b51122a50eeb76dbce4f17678f9
kuro <lu0130@outlook.com>
2020-01-09 00:14:18 +0800

    【摘要】新建自动化服务

---
+++ auto-service/README.md
@@ -0,0 +1,11 @@
+taozen(道禅)
+======
+auto-service 自动化服务
+------  
+> 所用技术栈
+> + Java11
+> + SpringBoot 2.2
+> + SpringCloud Hoxton
+> + Spring Cloud Alibaba Nacos
+> + Webflux 
+> + MongoDB
---
+++ auto-service/src/main/java/com/kuro/taozen/autoservice/AutoServiceApplication.java
@@ -0,0 +1,15 @@
+package com.kuro.taozen.autoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class AutoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoServiceApplication.class, args);
+    }
+
+}
