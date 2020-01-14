971d1dcc84bcccbab3ef9b69fa925ce89f63effa
kuro <lu0130@outlook.com>
2020-01-15 02:04:57 +0800

    【摘要】发号器响应式实现

--- identifier-service/src/main/java/com/kuro/taozen/handler/IdentifierHandler.java
+++ identifier-service/src/main/java/com/kuro/taozen/handler/IdentifierHandler.java
@@ -1,8 +1,7 @@
 package com.kuro.taozen.handler;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.support.atomic.RedisAtomicLong;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -10,7 +9,6 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import javax.annotation.Resource;
-import java.util.Objects;
 
 /**
  * 发号器处理器
@@ -22,13 +20,12 @@ import java.util.Objects;
 @Component
 public class IdentifierHandler {
     @Resource
-    private RedisTemplate redisTemplate;
+    private ReactiveRedisTemplate reactiveRedisTemplate;
 
     public Mono<ServerResponse> getOne(ServerRequest request) {
         return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(
-                Mono.justOrEmpty(new RedisAtomicLong(
-                        request.queryParam("type").orElse("undefined"),
-                        Objects.requireNonNull(redisTemplate.getConnectionFactory())
-                ).getAndIncrement()).defaultIfEmpty(0L), Long.class);
+                reactiveRedisTemplate.opsForValue()
+                        .increment(request.queryParam("type").orElse("undefined"), 1L)
+                        .defaultIfEmpty(0L), Long.class);
     }
 }
