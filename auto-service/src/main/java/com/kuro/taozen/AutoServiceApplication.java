37f4506bf551c4aa02053f347db9ca1667522132
kuro <lu0130@outlook.com>
2020-01-15 21:26:16 +0800

    【摘要】异步响应式负载均衡客户端构造器

--- auto-service/src/main/java/com/kuro/taozen/AutoServiceApplication.java
+++ auto-service/src/main/java/com/kuro/taozen/AutoServiceApplication.java
@@ -3,6 +3,9 @@ package com.kuro.taozen;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -12,4 +15,9 @@ public class AutoServiceApplication {
         SpringApplication.run(AutoServiceApplication.class, args);
     }
 
+    @Bean
+    @LoadBalanced
+    public WebClient.Builder clientBuilder(){
+        return WebClient.builder();
+    }
 }
