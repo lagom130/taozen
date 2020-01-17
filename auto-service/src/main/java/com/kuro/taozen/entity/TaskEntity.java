c9ecb199a7d20264b5dc40f0e80c7ead8f599293
kuro <lu0130@outlook.com>
2020-01-17 23:54:03 +0800

    【摘要】任务实体设计

--- auto-service/src/main/java/com/kuro/taozen/entity/TaskEntity.java
+++ auto-service/src/main/java/com/kuro/taozen/entity/TaskEntity.java
@@ -1,10 +1,25 @@
 package com.kuro.taozen.entity;
 
+import lombok.Data;
+
+import java.util.List;
+
 /**
  * 任务实体
  *
  * @author kuro
  * @create 2020-01-09
  **/
+@Data
 public class TaskEntity {
+
+    private String id;
+    private String tid;
+    private String jobId;
+    private String jobName;
+    private List<String> hosts;
+    private Long startTime;
+    private Long finishTime;
+    private String type;
+    private String status;
 }
