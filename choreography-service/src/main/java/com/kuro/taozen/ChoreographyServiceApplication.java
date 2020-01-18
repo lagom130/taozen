99deba2dbd76b59a931553013728a14ae264fb95
kuro <lu0130@outlook.com>
2020-01-18 23:08:48 +0800

    【摘要】编排服务初始化

---
+++ choreography-service/src/main/java/com/kuro/taozen/ChoreographyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kuro.taozen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoreographyServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChoreographyServiceApplication.class, args);
+    }
+
+}
